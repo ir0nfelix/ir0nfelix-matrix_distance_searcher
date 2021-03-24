c7f8c40f0b8a3124db73237d65693b36c580071e
iron_felix <johnzorn1988@gmail.com>
2021-03-25 02:11:05 +0300

    RachkovskySearcher() refactor

--- matrix_distance_searcher.py
+++ matrix_distance_searcher.py
@@ -2,10 +2,22 @@ import numpy as np
 
 
 class RachkovskySearcher:
-    def __init__(self, initial_matrix):
-        self.origin_d_matrix = initial_matrix
+    def __call__(self, initial_matrix_path: str):
+        return self.get_distance_matrix(initial_matrix_path)
 
-    def _generate_r_matrix(self, input_matrix: np.ndarray) -> np.ndarray:
+    def get_distance_matrix(self, initial_matrix_path: str) -> np.ndarray:
+        origin_d_matrix = np.load(initial_matrix_path)
+        return self._get_matrix_compare(origin_d_matrix)
+
+    def _get_matrix_compare(self, current_d_matrix: np.ndarray):
+        next_d_matrix = self._generate_d_matrix(current_d_matrix)
+        while not np.array_equal(next_d_matrix, current_d_matrix):
+            return self._get_matrix_compare(next_d_matrix)
+        else:
+            return current_d_matrix
+
+    @staticmethod
+    def _generate_r_matrix(input_matrix: np.ndarray) -> np.ndarray:
         r_matrix_obj = np.vectorize(lambda d: 1 if d else 0)
         r_matrix = r_matrix_obj(input_matrix)
         np.fill_diagonal(r_matrix, 1)
@@ -15,17 +27,19 @@ class RachkovskySearcher:
     def _generate_d_matrix(self, input_matrix: np.ndarray) -> np.ndarray:
         r_matrix = self._generate_r_matrix(input_matrix)
         index_pairs = [
-            (i, j) for i in range(0, input_matrix.shape[0]) for j in range(0, input_matrix.shape[1]) if i < j
+            (i, j) for i in range(0, input_matrix.shape[0])
+            for j in range(0, input_matrix.shape[1]) if i < j
         ]
 
-        new_matrix = np.empty_like(input_matrix)
+        d_matrix = np.empty_like(input_matrix)
         for pair in index_pairs:
             i, j = pair
             sum_list = [
-                r_matrix[j, row] * input_matrix[i, row] + r_matrix[i, row] * input_matrix[j, row]
-                for row in range(0, input_matrix.shape[1])
+                r_matrix[j, k] * input_matrix[i, k] + r_matrix[i, k] * input_matrix[j, k]
+                for k in range(0, input_matrix.shape[1])
             ]
-            new_matrix[i, j] = new_matrix[j, i] = 0 if set(sum_list) == set([0]) else min(sum_list)
-            np.fill_diagonal(new_matrix, 0)
 
-        return new_matrix
+            d_matrix[i, j] = d_matrix[j, i] = 0 if {*sum_list} == {0} else min({*sum_list}-{0})
+            np.fill_diagonal(d_matrix, 0)
+
+        return d_matrix
